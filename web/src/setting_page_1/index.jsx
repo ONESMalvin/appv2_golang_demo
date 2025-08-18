8b8be9832a6ece3b814298c4e55471615f2f027c
wangxuan-ones <wangxuan@ones.ai>
2025-08-18 15:14:53 +0800

    test

--- web/src/setting_page_1/index.jsx
+++ web/src/setting_page_1/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Table } from "@ones-design/table";
+import { ConfigProvider, Select } from '@ones-design/core'
 import { useMemoizedFn } from "ahooks";
 import ReactDOM from "react-dom";
 
@@ -8,6 +9,8 @@ const App = () => {
     const [token, setToken] = useState("");
     const [teams, setTeams] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [teamUsers, setTeamUsers] = useState([]);
+    const [projects, setProjects] = useState([]);
 
     useEffect(() => {
         document.dispatchEvent(new CustomEvent("ones:platform:client:sdk", {
@@ -24,7 +27,7 @@ const App = () => {
 
     const fetchTeams = useMemoizedFn(async () => {
         setLoading(true);
-        const res = await SDK.ONES.fetch("/account/teams", {
+        const res = await SDK.ONES.fetch("/openapi/v2/account/teams", {
             headers: {
                 "Authorization": `Bearer ${token}`
             }
@@ -32,6 +35,7 @@ const App = () => {
         const data = await res.json();
 
         const teams = data?.data?.teams || [];
+        console.log(teams);
         teams.forEach(team => {
             team.createdAt = new Date(team.createTime/1000).toLocaleString("zh-CN", {
                 year: 'numeric',
@@ -43,6 +47,42 @@ const App = () => {
         });
         setTeams(teams);
         setLoading(false);
+    })
+
+    const onTeamSelect = useMemoizedFn(async (value, opt) => {
+        fetchTeamUsers(value);
+        fetchProjects(value);
+    })
+
+    const fetchTeamUsers = useMemoizedFn(async (value) => {
+        const res = await SDK.ONES.fetch(`/openapi/v2/account/users/search?teamID=${value}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        const data = await res.json();
+        const users = data?.data?.list || [];
+        setTeamUsers(users);
+    })
+
+    const fetchProjects = useMemoizedFn(async (value) => {
+        const res = await SDK.ONES.fetch(`/openapi/v2/project/projects?teamID=${value}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        const data = await res.json();
+        const projects = data?.data?.list || [];
+        projects.forEach(project => {
+            project.createdAt = new Date(project.createTime/1000).toLocaleString("zh-CN", {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+            });
+        });
+        setProjects(projects);
     })
 
     return (
@@ -75,11 +115,59 @@ const App = () => {
                 ]}
                 dataSource={teams}
             />
+            <h2>Select a team</h2>
+            <Select options={teams.map(team => ({
+                label: team.name,
+                value: team.id
+            }))} onChange={onTeamSelect} onSelect={function Ua() {}}/>
+            <Table
+                columns={[
+                    {
+                        dataIndex: 'id',
+                        lock: true,
+                        with: 80,
+                        name:'用户UUID'
+                    },
+                    {
+                        dataIndex: 'name',
+                        name: '用户名称',
+                        width: 80,
+                    },
+                    {
+                        dataIndex: 'email',
+                        name: '邮箱',
+                        width: 150,
+                    },
+                ]}
+                dataSource={teamUsers}
+            />
+            <h2>Projects</h2>
+            <Table
+                columns={[
+                    {
+                        dataIndex: 'id',
+                        with: 80,
+                        name: '项目UUID'
+                    },
+                    {
+                        dataIndex: 'name',
+                        name: '项目名称'
+                    },
+                    {
+                        dataIndex: 'createdAt',
+                        name: '创建时间',
+                        width: 150
+                    }
+                ]}
+                dataSource={projects}
+            />
         </div>
     )
 }
 
 ReactDOM.render(
-    <App />,
+    <ConfigProvider>
+        <App />
+    </ConfigProvider>,
     document.body.querySelector('#app')
 )
